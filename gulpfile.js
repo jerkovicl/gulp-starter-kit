60db6ea1d6c5e9f2e0cacff36f52a747c31c3c52
unknown <sime@SIME-PC.sistemi.corp>
2015-11-20 11:41:42 +0100

    changes

--- gulpfile.js
+++ gulpfile.js
@@ -337,7 +337,7 @@ gulp.task('build:otherjs', function() {
 /*******************************************************************************
     COMPILE LESS TO CSS
 *******************************************************************************/
-// TODO Replace LESS with Sass / Scss
+// TODO Replace LESS with Sass / SCSS
 gulp.task('less', function() {
   return gulp.src('styles/main.less')
     .pipe(less())
